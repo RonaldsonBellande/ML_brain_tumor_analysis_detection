a711b02ef893796677c328393d5e0db9e6e89b4e
RonaldsonBellande <ronaldsonbellande@gmail.com>
2022-01-18 16:05:43 -0500

    predict

--- run_all.sh
+++ run_all.sh
@@ -25,19 +25,19 @@
  # python3 brain_tumor_analysis.py model_training 4 model3 edge_2
 
 
-# python3 brain_tumor_analysis.py image_prediction 2 
-# python3 brain_tumor_analysis.py image_prediction 2 
-# python3 brain_tumor_analysis.py image_prediction 2 
+python3 brain_tumor_analysis.py image_prediction 2 model1
+python3 brain_tumor_analysis.py image_prediction 2 model2
+python3 brain_tumor_analysis.py image_prediction 2 model3
 
-# python3 brain_tumor_analysis.py image_prediction 4 
-# python3 brain_tumor_analysis.py image_prediction 4 
-# python3 brain_tumor_analysis.py image_prediction 4 
+python3 brain_tumor_analysis.py image_prediction 4 model1
+python3 brain_tumor_analysis.py image_prediction 4 model2
+# python3 brain_tumor_analysis.py image_prediction 4 model3
 
 
-python3 brain_tumor_analysis.py transfer_learning 2 model1 normal 10
-python3 brain_tumor_analysis.py transfer_learning 2 model2 normal 10
-python3 brain_tumor_analysis.py transfer_learning 2 model3 normal 10
+# python3 brain_tumor_analysis.py transfer_learning 2 model1 normal 10
+# python3 brain_tumor_analysis.py transfer_learning 2 model2 normal 10
+# python3 brain_tumor_analysis.py transfer_learning 2 model3 normal 10
 
-python3 brain_tumor_analysis.py transfer_learning 4 model1 normal 10
-python3 brain_tumor_analysis.py transfer_learning 4 model2 normal 10
+# python3 brain_tumor_analysis.py transfer_learning 4 model1 normal 10
+# python3 brain_tumor_analysis.py transfer_learning 4 model2 normal 10
 # python3 brain_tumor_analysis.py transfer_learning 4 model3 normal
