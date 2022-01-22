402e196a2506aa8f9264590f45fddc6ce3d0e203
RonaldsonBellande <ronaldsonbellande@gmail.com>
2022-01-22 09:15:09 -0500

    let's see

--- computer_vision_model_detection.py
+++ computer_vision_model_detection.py
@@ -11,18 +11,15 @@ class computer_vision_localization_detection(object):
 
         self.image_size = 240
         self.number_classes = int(number_classes)
-        self.number_images_to_plot = 900
         self.split_size = 900
 
         self.graph_path = "graph_charts/" + "detection_localization/"
 
         if self.number_classes == 2:
-            self.number_images_to_plot = 9
             self.model_categpory = ["False","True"]
             self.image_path = "brain_cancer_category_2/" + "Testing2/" 
        
         elif self.number_classes == 4:
-            self.number_images_to_plot = 16
             self.model_categpory = ["False", "glioma_tumor", "meningioma_tumor", "pituitary_tumor"]
             self.image_path = "brain_cancer_category_4/" + "Testing2/" 
 
@@ -36,27 +33,29 @@ class computer_vision_localization_detection(object):
             image_resized = cv2.imread(os.path.join(self.image_path, image))
             image_resized = cv2.resize(image_resized,(self.image_size, self.image_size), interpolation = cv2.INTER_AREA)
             
-            self.image_file.append(image_resized)
+            self.split_images(image_resized)
         
-        print(self.image_file)
         self.image_file = np.array(self.image_file)
         self.X_test = self.image_file.astype("float32") / 255
     
 
     def split_images(self, image):
 
-        for r in range(0,img.shape[0],30):
-            for c in range(0,img.shape[1],30):
-                cv2.imwrite(f"img{r}_{c}.png",img[r:r+30, c:c+30,:])
+        for r in range(0,image.shape[0],int(math.sqrt(self.split_size))):
+            for c in range(0,image.shape[1],int(math.sqrt(self.split_size))):
+                
+                image_split = image[r:r+int(math.sqrt(self.split_size)), c:c+int(math.sqrt(self.split_size)),:]
+                image_split = cv2.resize(image_split,(self.image_size, self.image_size), interpolation = cv2.INTER_AREA)
+                self.image_file.append(image_split)
 
 
     def plot_prediction_with_model(self):
 
-        plt.figure(dpi=1000)
+        plt.figure(dpi=10000)
         predicted_classes = self.model.predict(self.X_test)
 
-        for i in range(self.number_images_to_plot):
-            plt.subplot(math.sqrt(self.number_images_to_plot),math.sqrt(self.number_images_to_plot),i+1)
+        for i in range(self.split_size):
+            plt.subplot(math.sqrt(self.split_size),math.sqrt(self.split_size),i+1)
             fig=plt.imshow(self.X_test[i,:,:,:])
             plt.axis('off')
             plt.title("Predicted - {}".format(self.model_categpory[np.argmax(predicted_classes[i], axis=0)]), fontsize=1)
