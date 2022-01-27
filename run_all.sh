c640bf853f5b23ca56c5aadce4da7b934a75936d
RonaldsonBellande <ronaldsonbellande@gmail.com>
2022-01-26 20:37:02 -0500

    ok

--- run_all.sh
+++ run_all.sh
@@ -27,11 +27,11 @@
 
 python3 brain_tumor_analysis.py image_prediction 2 model1
 python3 brain_tumor_analysis.py image_prediction 2 model2
-python3 brain_tumor_analysis.py image_prediction 2 model3
+# python3 brain_tumor_analysis.py image_prediction 2 model3
 
-python3 brain_tumor_analysis.py image_prediction 4 model1
-python3 brain_tumor_analysis.py image_prediction 4 model2
-python3 brain_tumor_analysis.py image_prediction 4 model3
+# python3 brain_tumor_analysis.py image_prediction 4 model1
+# python3 brain_tumor_analysis.py image_prediction 4 model2
+# python3 brain_tumor_analysis.py image_prediction 4 model3
 
 
 # python3 brain_tumor_analysis.py transfer_learning 2 model1 normal 10
