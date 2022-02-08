d5eb28f92c4c79d1d51f54688350798a26c86b82
RonaldsonBellande <ronaldsonbellande@gmail.com>
2022-02-08 15:38:18 -0500

    ok

--- computer_vision_model_localization_detection.py
+++ computer_vision_model_localization_detection.py
@@ -1,13 +1,5 @@
 from header_imports import *
 
-label_map = {
-    "False": "blue",
-    "True": "yellow", 
-    "glioma_tumor": "red",
-    "meningioma_tumor": "green",
-    "pituitary_tumor": "white", 
-}
-
 class computer_vision_localization_detection(object):
     def __init__(self, save_model, number_classes):
         
@@ -25,6 +17,15 @@ class computer_vision_localization_detection(object):
         self.classes = None
         self.label = -1
         self.score = -1
+        self.number_box = 3
+
+        self.label_map = {
+            "False": "blue",
+            "True": "yellow", 
+            "glioma_tumor": "red",
+            "meningioma_tumor": "green",
+            "pituitary_tumor": "white", 
+            }
 
         self.class_threshold = 0.3
         self.class_threshold_max = 0.5
@@ -66,7 +67,15 @@ class computer_vision_localization_detection(object):
         return self.label
 
 
-    def get_score(self):
+    def get_score(self, xmin, ymin, xmax, ymax, objness, classes):
+
+        self.xmin = xmin
+        self.ymin = ymin
+        self.xmax = xmax
+        self.ymax = ymax
+        self.objness = objness
+        self.classes = classes
+
         if self.score == -1:
             self.score = self.classes[self.get_label()]
         return self.score
@@ -74,9 +83,9 @@ class computer_vision_localization_detection(object):
 
     def decode_netout(self, predicted_classes_network, anchors):
         grid_h, grid_w = predicted_classes_network.shape[:2]
-        nb_box = 3
-        predicted_classes_network = predicted_classes_network.reshape((grid_h, grid_w, nb_box, -1))
+        predicted_classes_network = predicted_classes_network.reshape((grid_h, grid_w, self.number_box, -1))
         nb_class = predicted_classes_network.shape[-1] - 5
+
         boxes = []
         predicted_classes_network[..., :2]  = 1.0 / (1.0 + np.exp(-(predicted_classes_network[..., :2])))
         predicted_classes_network[..., 4:]  = 1.0 / (1.0 + np.exp(-(predicted_classes_network[...,4:])))
@@ -89,7 +98,6 @@ class computer_vision_localization_detection(object):
             for b in range(nb_box):
                 # 4th element is objectness score
                 objectness = predicted_classes_network[int(row)][int(col)][b][4]
-            
                 if(objectness.all() <= self.class_threshold = 0.3): continue
                 x, y, w, h = predicted_classes_network[int(row)][int(col)][b][:4]
                 x = (col + x) / grid_w 
@@ -97,25 +105,41 @@ class computer_vision_localization_detection(object):
                 w = anchors[2 * b + 0] * np.exp(w) / self.image_size
                 h = anchors[2 * b + 1] * np.exp(h) / self.image_size
                 classes = predicted_classes_network[int(row)][col][b][5:]
-                box = BoundBox(x-w/2, y-h/2, x+w/2, y+h/2, objectness, classes)
+                box = self.get_score(x-w/2, y-h/2, x+w/2, y+h/2, objectness, classes)
                 boxes.append(box)
         return boxes
  
 
-    def correct_yolo_boxes(self, boxes, image_h, image_w, net_h, net_w):
-        new_w, new_h = net_w, net_h
+    def correct_yolo_boxes(self, boxes):
+
         for i in range(len(boxes)):
-            x_offset, x_scale = (net_w - new_w)/2./net_w, float(new_w)/net_w
-            y_offset, y_scale = (net_h - new_h)/2./net_h, float(new_h)/net_h
+            x_offset, x_scale = (self.image_size - self.image_size)/2.0/self.image_size, float(self.image_size)/self.image_size
+            y_offset, y_scale = (self.image_size - self.image_size)/2.0/self.image_size, float(self.image_size)/self.image_size
             boxes[i].xmin = int((boxes[i].xmin - x_offset) / x_scale * self.image_size)
             boxes[i].xmax = int((boxes[i].xmax - x_offset) / x_scale * self.image_size)
             boxes[i].ymin = int((boxes[i].ymin - y_offset) / y_scale * self.image_size)
             boxes[i].ymax = int((boxes[i].ymax - y_offset) / y_scale * self.image_size)
 
+        if len(boxes) > 0:
+            nb_class = len(boxes[0].classes)
+        else:
+            return
 
-    def _interval_overlap(interval_a, interval_b):
+        for c in range(nb_class):
+            sorted_indices = np.argsort([-box.classes[c] for box in boxes])
+            for i in range(len(sorted_indices)):
+                index_i = sorted_indices[i]
+                if boxes[index_i].classes[c] == 0: continue
+                for j in range(i+1, len(sorted_indices)):
+                    index_j = sorted_indices[j]
+                    if bbox_iou(boxes[index_i], boxes[index_j]) >= self.class_threshold_max:
+                        boxes[index_j].classes[c] = 0
+
+
+    def interval_overlap(interval_a, interval_b):
         x1, x2 = interval_a
         x3, x4 = interval_b
+
         if x3 < x1:
             if x4 < x1:
                 return 0
@@ -129,8 +153,8 @@ class computer_vision_localization_detection(object):
 
 
     def bbox_iou(box1, box2):
-        intersect_w = _interval_overlap([box1.xmin, box1.xmax], [box2.xmin, box2.xmax])
-        intersect_h = _interval_overlap([box1.ymin, box1.ymax], [box2.ymin, box2.ymax])
+        intersect_w = self.interval_overlap([box1.xmin, box1.xmax], [box2.xmin, box2.xmax])
+        intersect_h = self.interval_overlap([box1.ymin, box1.ymax], [box2.ymin, box2.ymax])
         intersect = intersect_w * intersect_h
         w1, h1 = box1.xmax-box1.xmin, box1.ymax-box1.ymin
         w2, h2 = box2.xmax-box2.xmin, box2.ymax-box2.ymin
@@ -138,25 +162,8 @@ class computer_vision_localization_detection(object):
         return float(intersect) / union
 
 
-    def do_nms(boxes, nms_thresh):
-        if len(boxes) > 0:
-            nb_class = len(boxes[0].classes)
-        else:
-            return
-
-        for c in range(nb_class):
-            sorted_indices = np.argsort([-box.classes[c] for box in boxes])
-            for i in range(len(sorted_indices)):
-                index_i = sorted_indices[i]
-                if boxes[index_i].classes[c] == 0: continue
-                for j in range(i+1, len(sorted_indices)):
-                    index_j = sorted_indices[j]
-                    if bbox_iou(boxes[index_i], boxes[index_j]) >= nms_thresh:
-                        boxes[index_j].classes[c] = 0
-
-
     # get all of the results above a threshold
-    def get_boxes(boxes):
+    def draw_boxes(boxes):
         v_boxes, v_labels, v_scores = list(), list(), list()
         for box in boxes:
             # enumerate all possible labels
@@ -166,11 +173,7 @@ class computer_vision_localization_detection(object):
                     v_boxes.append(box)
                     v_labels.append(self.model_categpory[i])
                     v_scores.append(box.classes[i]*100)
-        return v_boxes, v_labels, v_scores
 
-
-    # draw all results
-    def draw_boxes(filename, v_boxes, v_labels, v_scores):
 
         pyplot.imshow(pyplot.imread(filename))
         ax = pyplot.gca()
@@ -178,101 +181,30 @@ class computer_vision_localization_detection(object):
             box = v_boxes[i]
             y1, x1, y2, x2 = box.ymin, box.xmin, box.ymax, box.xmax
             width, height = x2 - x1, y2 - y1
-            rect = Rectangle((x1, y1), width, height, fill=False, color=label_map[v_labels[i]])
+            rect = Rectangle((x1, y1), width, height, fill=False, color=self.label_map[v_labels[i]])
             ax.add_patch(rect)
             label = "%s (%.3f)" % (v_labels[i], v_scores[i])
-            pyplot.text(x1, y1, label, color=label_map[v_labels[i]])
+            pyplot.text(x1, y1, label, color=self.label_map[v_labels[i]])
         pyplot.show()
-    
 
-    # def prepare_image_data(self):
-    #     
-    #     for image in os.listdir(self.image_path):
-    #         image_resized = cv2.imread(os.path.join(self.image_path, image))
-    #         image_resized = cv2.resize(image_resized,(self.image_size, self.image_size), interpolation = cv2.INTER_AREA)
-    #         self.split_images(image_resized)
-    #     
-    #     self.image_file = np.array(self.image_file)
-    #     self.X_test = self.image_file.astype("float32") / 255
-    
-
-    # def split_images(self, image):
-
-    #     for r in range(0,image.shape[0],int(math.sqrt(self.split_size))):
-    #         for c in range(0,image.shape[1],int(math.sqrt(self.split_size))):
-    #             image_split = image[r:r+int(math.sqrt(self.split_size)), c:c+int(math.sqrt(self.split_size)),:]
-    #             image_split = cv2.resize(image_split,(self.image_size, self.image_size), interpolation = cv2.INTER_AREA)
-    #             self.image_file.append(image_split)
-
-
-    # def plot_prediction_with_model(self):
-
-    #     predicted_classes = self.model.predict(self.X_test)
-
-    #     for i in range(len(self.image_file)):
-    #         if self.number_classes == 2:
-    #             self.predicted_classes_array.append([np.argmax(predicted_classes[i])][0])
-    #         elif self.number_classes == 4:
-    #             self.predicted_classes_array.append([np.argmax(predicted_classes[i])][0])
-
-    #     self.predicted_classes_array = np.reshape(self.predicted_classes_array, ((int(math.sqrt(len(self.image_file)))), (int(math.sqrt(len(self.image_file))))))
-    
-
-    # def prepare_prediction(self):
-    #     
-    #     self.image_file_image = []
-    #     for image in os.listdir(self.image_path):
-    #         image_resized = cv2.imread(os.path.join(self.image_path, image))
-    #         image_resized = cv2.resize(image_resized,(self.image_size, self.image_size), interpolation = cv2.INTER_AREA)
-    #         self.image_file_image.append(image_resized)
-    #     
-    #     self.image_file_image = np.array(self.image_file_image)
-    #     self.X_test_image = self.image_file_image.astype("float32") / 255
-    #     self.predicted_classes = self.model.predict(self.X_test_image)
-    
-    # 
-    # def predict_parts_images(self, index):
-    #     
-    #     image_box = []
-    #     for image in os.listdir(self.image_path):
-    #         image_resized = cv2.imread(os.path.join(self.image_path, image))
-    #         image_resized = cv2.resize(image_resized,(self.image_size, self.image_size), interpolation = cv2.INTER_AREA)
-    #         image_box.append(image_resized)
-    #     
-    #     image_box = np.array(image_box)
-    #     X_test_image_box = image_box.astype("float32") / 255
-    #     predicted_classes = self.model.predict(X_test_image_box)
-    #     return predicted_classes
-        
 
     def localization(self):
 
         for image in os.listdir(self.image_path):
             image_name = DATA_PATH + 'train_images/' + image
     
-            image, image_w, image_h = self.load_image_pixels(image_name)
-
             image_resized = cv2.imread(os.path.join(self.image_path, image))
             image_resized = cv2.resize(image_resized,(self.image_size, self.image_size), interpolation = cv2.INTER_AREA)
             image_resized = image_resized.astype('float32') / 255.0
             image_resized = expand_dims(image_resized, 0)
- 
             predicted_classes = self.model.predict(image_resized)
     
             boxes = list()
             for i in range(len(predicted_classes)):
-                boxes += self.decode_netout(predicted_classes[i][0], anchors[i], class_threshold)
+                boxes += self.decode_netout(predicted_classes[i][0], self.anchors[i])
 
-            self.correct_yolo_boxes(boxes, image_h, image_w)
-
-            # suppress non-maximal boxes
-            self.do_nms(boxes, 0.5)
-
-            # get the details of the detected objects
-            v_boxes, v_labels, v_scores = self.get_boxes(boxes, class_threshold)
-
-            # draw what we found
-            self.draw_boxes(image_name, v_boxes, v_labels, v_scores)
+            self.correct_yolo_boxes(boxes)
+            self.draw_boxes(boxes, class_threshold)
 
 
         
