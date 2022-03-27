0be1031e63f90c4ae510194ae601663d8d3be919
RonaldsonBellande <ronaldsonbellande@gmail.com>
2022-03-26 20:33:36 -0400

    ok

--- run_all.sh
+++ run_all.sh
@@ -61,34 +61,35 @@
 # python3 computer_vision_analysis.py segmentation 4 model3
 
 
-# python3 computer_vision_analysis.py continuous_learning  2 model1 model1 deep_q_learning
-# python3 computer_vision_analysis.py continuous_learning  2 model2 model2 deep_q_learning
-# python3 computer_vision_analysis.py continuous_learning  2 model3 model3 deep_q_learning
+python3 computer_vision_analysis.py continuous_learning 2 model1 model1 deep_q_learning
+# python3 computer_vision_analysis.py continuous_learning 2 model2 model2 deep_q_learning
+# python3 computer_vision_analysis.py continuous_learning 2 model3 model3 deep_q_learning
 
-# python3 computer_vision_analysis.py continuous_learning  2 model1 model1 double_deep_q_learning
-# python3 computer_vision_analysis.py continuous_learning  2 model2 model2 double_deep_q_learning
-# python3 computer_vision_analysis.py continuous_learning  2 model3 model3 double_deep_q_learning 
+# python3 computer_vision_analysis.py continuous_learning 2 model1 model1 double_deep_q_learning
+# python3 computer_vision_analysis.py continuous_learning 2 model2 model2 double_deep_q_learning
+# python3 computer_vision_analysis.py continuous_learning 2 model3 model3 double_deep_q_learning 
 
-# python3 computer_vision_analysis.py continuous_learning  2 model1 model1 dueling_deep_q_learning
-# python3 computer_vision_analysis.py continuous_learning  2 model2 model2 dueling_deep_q_learning
-# python3 computer_vision_analysis.py continuous_learning  2 model3 model3 dueling_deep_q_learning 
+# python3 computer_vision_analysis.py continuous_learning 2 model1 model1 dueling_deep_q_learning
+# python3 computer_vision_analysis.py continuous_learning 2 model2 model2 dueling_deep_q_learning
+# python3 computer_vision_analysis.py continuous_learning 2 model3 model3 dueling_deep_q_learning 
 
-# python3 computer_vision_analysis.py segmentation  2 model1 model1
-# python3 computer_vision_analysis.py segmentation  2 model2 model2
-# python3 computer_vision_analysis.py segmentation  2 model3 model3
 
-# python3 computer_vision_analysis.py continuous_learning  4 model1 model1 deep_q_learning
-# python3 computer_vision_analysis.py continuous_learning  4 model2 model2 deep_q_learning
-# python3 computer_vision_analysis.py continuous_learning  4 model3 model3 deep_q_learning
+# python3 computer_vision_analysis.py segmentation 2 model1 model1
+# python3 computer_vision_analysis.py segmentation 2 model2 model2
+# python3 computer_vision_analysis.py segmentation 2 model3 model3
 
-# python3 computer_vision_analysis.py continuous_learning  4 model1 model1 double_deep_q_learning
-# python3 computer_vision_analysis.py continuous_learning  4 model2 model2 double_deep_q_learning
-# python3 computer_vision_analysis.py continuous_learning  4 model3 model3 double_deep_q_learning 
+# python3 computer_vision_analysis.py continuous_learning 4 model1 model1 deep_q_learning
+# python3 computer_vision_analysis.py continuous_learning 4 model2 model2 deep_q_learning
+# python3 computer_vision_analysis.py continuous_learning 4 model3 model3 deep_q_learning
 
-# python3 computer_vision_analysis.py continuous_learning  4 model1 model1 dueling_deep_q_learning
-# python3 computer_vision_analysis.py continuous_learning  4 model2 model2 dueling_deep_q_learning
-# python3 computer_vision_analysis.py continuous_learning  4 model3 model3 dueling_deep_q_learning 
+# python3 computer_vision_analysis.py continuous_learning 4 model1 model1 double_deep_q_learning
+# python3 computer_vision_analysis.py continuous_learning 4 model2 model2 double_deep_q_learning
+# python3 computer_vision_analysis.py continuous_learning 4 model3 model3 double_deep_q_learning 
 
-# python3 computer_vision_analysis.py segmentation  4 model1 model1
-# python3 computer_vision_analysis.py segmentation  4 model2 model2
-# python3 computer_vision_analysis.py segmentation  4 model3 model3
+# python3 computer_vision_analysis.py continuous_learning 4 model1 model1 dueling_deep_q_learning
+# python3 computer_vision_analysis.py continuous_learning 4 model2 model2 dueling_deep_q_learning
+# python3 computer_vision_analysis.py continuous_learning 4 model3 model3 dueling_deep_q_learning 
+
+# python3 computer_vision_analysis.py segmentation 4 model1 model1
+# python3 computer_vision_analysis.py segmentation 4 model2 model2
+# python3 computer_vision_analysis.py segmentation 4 model3 model3
