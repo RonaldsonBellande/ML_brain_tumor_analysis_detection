b658f338fa0582e5e13d69ccca4dd3a8acc2eb41
RonaldsonBellande <ronaldsonbellande@gmail.com>
2022-03-28 16:56:07 -0400

    ok

--- run_all.sh
+++ run_all.sh
@@ -92,15 +92,6 @@
 # python3 computer_vision_analysis.py continuous_learning 2 model3 model3 dueling_deep_q_learning 
 
 
-# python3 computer_vision_analysis.py segmentation 2 model1 model1
-# python3 computer_vision_analysis.py segmentation 2 model2 model2
-# python3 computer_vision_analysis.py segmentation 2 model3 model3
-
-# python3 computer_vision_analysis.py segmentation 4 model1 model1
-# python3 computer_vision_analysis.py segmentation 4 model2 model2
-# python3 computer_vision_analysis.py segmentation 4 model3 model3
-
-
 # python3 computer_vision_analysis.py continuous_learning 4 model1 model1 deep_q_learning
 # python3 computer_vision_analysis.py continuous_learning 4 model2 model2 deep_q_learning
 # python3 computer_vision_analysis.py continuous_learning 4 model3 model3 deep_q_learning
